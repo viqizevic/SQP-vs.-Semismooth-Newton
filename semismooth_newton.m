d22c97a6faaf5e9453778c4f5568d9a06c8a573e
vicky hartanto tanzil <vicky.tanzil@googlemail.com>
2012-01-25 17:45:46 +0100

    Copy of newton method and create the one dimensional projection function

---
+++ semismooth_newton.m
@@ -0,0 +1,57 @@
+## Funktion: [x, it] = semismooth_newton(f, gradf, hessf, lamda, a, b, x0, itmax, tol)
+##
+##  Semismooth-Newton-Verfahren loest das Problem
+##        min ( f(x) + (lambda/2)*|x|^2 )
+##         x
+##        mit Nebenbedingung a <= x <= b
+##
+##  f : R^n -> R
+##  lambda eine reelle Zahl
+##  a und b aus R^n
+
+function [x, it] = semismooth_newton(f, gradf, hessf, lambda, a, b, x0, itmax, tol)
+
+	x = x0;
+	it = 0;
+	stop = false;
+	
+	## Ueberpruefe das Abbruchkriterium fuer x0
+	if( norm(feval(gradf,x)) < tol )
+		stop = true;
+	endif
+
+	while( ~stop )
+		it = it + 1;
+		d = feval(hessf,x) \ -feval(gradf,x);
+		%sigma = armijo_schrittweite(f, gradf, x, d, 0.75, 4, 0.5);
+		sigma = powell_schrittweite(f, gradf, x, d, 0.01, 0.9);
+		x = x + sigma*d;
+		% Stop criteria
+		if( norm(feval(gradf,x)) < tol )
+			stop = true;
+		endif
+
+		% Too many iteration
+		if( it >= itmax)
+			stop = true;
+		endif
+	endwhile
+
+endfunction
+
+
+
+
+function w = projection_one_dim(v, a, b)
+	if (a > b)
+		error ("a suppose to be smaller than b");
+	endif
+	
+	w = v;
+	if (v < a)
+		w = a;
+	elseif (b < v)
+		w = b;
+	endif
+
+endfunction
