8806c5184e5483f6746dd906840dd27165a204c3
vicky hartanto tanzil <vicky.tanzil@googlemail.com>
2012-02-06 07:10:23 +0100

    complete the test files for exp function

--- test/exp_func/hess_exp_func.m
+++ test/exp_func/hess_exp_func.m
@@ -1,3 +1,10 @@
 function H = hess_exp_func(x)
-	H = 2*eye(length(x));
+	n = length(x);
+	efx = exp_func(x);
+	H = 2*efx*eye(n);
+	for k=1:n
+		for l=1:n
+			H(k,l) += 4*efx*x(k)*x(l);
+		endfor
+	endfor
 endfunction
--- test/exp_func/test_exp_func.m
+++ test/exp_func/test_exp_func.m
@@ -1,9 +1,9 @@
-function test_quad_func()
+function test_exp_func()
 	lambda = 1;
-	a = -10;
-	b = 10;
+	a = -5;
+	b = 5;
 	x0 = 4;
 	itmax = 10;
 	tol = 0.01;
-	[x,it] = semismooth_newton('quad_func','grad_quad_func','hess_quad_func',lambda,a,b,x0,itmax,tol)
+	[x,it] = semismooth_newton('exp_func','grad_exp_func','hess_exp_func',lambda,a,b,x0,itmax,tol)
 endfunction
