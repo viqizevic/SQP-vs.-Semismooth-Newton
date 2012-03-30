3684e94f068e64fba05f008991d7be6b4220076b
Vicky H. Tanzil <tanzil@math.tu-berlin.de>
2012-03-30 17:13:55 +0200

    approx hessian small signs bugs

--- approx_hessian.m
+++ approx_hessian.m
@@ -7,7 +7,7 @@ function hessf = approx_hessian(f,x,epsi)
         for j=1:k
             ej = zeros(n,1);
             ej(j) = 1;
-            hessf(k,j) = ( feval(f,x+epsi*ek+epsi*ej) - feval(f,x-epsi*ek) - feval(f,x-epsi*ej) + feval(f,x) ) / (epsi^2);
+            hessf(k,j) = ( feval(f,x+epsi*ek+epsi*ej) - feval(f,x+epsi*ek) - feval(f,x+epsi*ej) + feval(f,x) ) / (epsi^2);
             if k == j
                 hessf(k,k) = hessf(k,k)/2;
             end
