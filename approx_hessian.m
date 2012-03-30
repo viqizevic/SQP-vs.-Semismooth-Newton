5c99369a8e771ddf369bb03bbd9d633989b6fe19
Vicky H. Tanzil <tanzil@math.tu-berlin.de>
2012-03-30 16:13:30 +0200

    add approximations functions for the gradient and the hessian

---
+++ approx_hessian.m
@@ -0,0 +1,17 @@
+function hessf = approx_hessian(f,x,epsi)
+    n = length(x);
+    hessf = zeros(n,n);
+    for k=1:n
+        ek = zeros(n,1);
+        ek(k) = 1;
+        for j=1:k
+            ej = zeros(n,1);
+            ej(j) = 1;
+            hessf(k,j) = ( feval(f,x+epsi*ek+epsi*ej) - feval(f,x-epsi*ek) - feval(f,x-epsi*ej) + feval(f,x) ) / (epsi^2);
+            if k == j
+                hessf(k,k) = hessf(k,k)/2;
+            end
+        end
+    end
+    hessf = hessf + hessf';
+end
