97ee96704020c54bff85288ba0ab9e735ed7d913
Vicky Hartanto Tanzil <vicky.tanzil@googlemail.com>
2012-04-17 22:35:42 +0200

    repair plot3dim function

--- extra/plot3dim.m
+++ extra/plot3dim.m
@@ -1,18 +1,21 @@
-% plot3dim(f,a,b,gap)
+% plot3dim(f,x1,x2,gapx,y1,y2,gapy)
 % - the function f should be a scalarfunction f:R^2->R
-% - a,b real numbers with a < b
-% - gap a real number with gap << (b-a)
+% - x1,x2 real numbers with x1 < x2
+% - gapx a real number with gapx << (x2-x1)
+% - y1,y2 real numbers with y1 < y2
+% - gapy a real number with gapy << (y2-y1)
 % the plot will be made considering all points (x,y):
-% x in {a, a+gap, a+2*gap, ... , b},
-% y in {a, a+gap, a+2*gap, ... , b}
+% x in {x1, x1+gapx, x1+2*gapx, ... , x2},
+% y in {y1, y1+gapy, y1+2*gapy, ... , y2}
 % 
-function plot3dim(f,a,b,gap)
-	vx = a:gap:b;
-	vy = vx;
+function plot3dim(f,x1,x2,gapx,y1,y2,gapy)
+	vx = x1:gapx:x2;
+	vy = y1:gapy:y2;
 	[x,y] = meshgrid(vx,vy);
 	[m,n] = size(x);
-	for k=1:n
-		for l=1:m
+	z = zeros(m,n);
+	for k=1:m
+		for l=1:n
 			v = [x(k,l); y(k,l)];
 			z(k,l) = feval(f,v);
 		end
