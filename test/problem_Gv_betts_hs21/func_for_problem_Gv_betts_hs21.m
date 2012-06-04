7b2f56b2458a3b12ee8d2f2ad7122ad14a749288
Vicky Hartanto Tanzil <vicky.tanzil@googlemail.com>
2012-06-04 20:31:35 +0200

    define the latex func defs in the problems.xml

--- test/problem_Gv_betts_hs21/func_for_problem_Gv_betts_hs21.m
+++ test/problem_Gv_betts_hs21/func_for_problem_Gv_betts_hs21.m
@@ -1,5 +1,5 @@
 function y = func_for_problem_Gv_betts_hs21(x)
-	Q = 2*[0.1 0; 0 1];
+	Q = 2*[0.01 0; 0 1];
 	q = [0; 0];
 	c = -100;
 	y = 0.5*x'*Q*x + q'*x + c;
--- test/problem_Gv_betts_hs21/grad_func_for_problem_Gv_betts_hs21.m
+++ test/problem_Gv_betts_hs21/grad_func_for_problem_Gv_betts_hs21.m
@@ -1,5 +1,5 @@
 function g = grad_func_for_problem_Gv_betts_hs21(x)
-	Q = 2*[0.1 0; 0 1];
+	Q = 2*[0.01 0; 0 1];
 	q = [0; 0];
 	c = -100;
 	g = Q*x + q;
