4e79f88af19df8845587cce2892b5c5b5527f15e
Vicky Hartanto Tanzil <vicky.tanzil@googlemail.com>
2012-06-25 14:11:30 +0200

    add some info about the test

--- test/extras/function_value_in_iteration_points.m
+++ test/extras/function_value_in_iteration_points.m
@@ -1,15 +1,28 @@
-function function_value_in_iteration_points(testproblem,func)
+function function_value_in_iteration_points()
+	%testproblem = 'test_problem_v_beale_1';
+	%func = 'func_for_problem_v_beale_1';
+	testproblem = 'test_problem_v_dixon';
+	func = 'func_for_problem_v_dixon';
 	[x_ssn,it_ssn,t_ssn,x_sqp,it_sqp,t_sqp,X_ssn,X_sqp] = feval(testproblem,2);
 	V = X_ssn;
+	W = X_sqp;
 	[m,n] = size(V);
-	for i=1:m
-		x = V(i,:)';
-		feval(func,x)
+	[p,n] = size(W);
+	r = max(m,p);
+	s = min(m,p);
+	for i=1:r
+		if i <= s
+			U(i,1) = feval(func,V(i,:)');
+			U(i,2) = feval(func,W(i,:)');
+		else
+			if m == r
+				U(i,1) = feval(func,V(i,:)');
+				U(i,2) = 1.23456789;
+			else
+				U(i,1) = 1.23456789;
+				U(i,2) = feval(func,W(i,:)');
+			end
+		end
 	end
-	V = X_sqp;
-	[m,n] = size(V);
-	for i=1:m
-		x = V(i,:)';
-		feval(func,x)
-	end
+	U
 end
